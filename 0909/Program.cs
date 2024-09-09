b765bc5e4caa51822e0498cd81f3bcc195a7d127
Tamás Hegedűs <damien.ramenov@gmail.com>
2024-09-09 14:59:17 +0200

    Add project files.

---
+++ 0909/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _0909
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Adj meg egy számot:");
+
+            var inputNumberText = Console.ReadLine();
+
+            var conversionSuccessful = int.TryParse(inputNumberText, out var inputNumber);
+            if (!conversionSuccessful)
+            {
+                Console.WriteLine("Gonosz voltál, mert nem számot adtál meg! :(");
+            }
+
+            if (inputNumber % 3 == 0)
+            {
+                Console.WriteLine("Hárommal osztható a megadott szám!");
+            }
+            else if (inputNumber % 4 == 0)
+            {
+                Console.WriteLine("Néggyel osztható a megadott szám!");
+            }
+            else if (inputNumber % 9 == 0)
+            {
+                Console.WriteLine("Kilenccel osztható a megadott szám!");
+            }
+            else
+            {
+                Console.WriteLine("Nem osztható hárommal, néggyel, vagy kilenccel a megadott szám!");
+            }
+        }
+    }
+}
